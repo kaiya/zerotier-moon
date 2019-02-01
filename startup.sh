5d597fe3497f2c373848d818f1e4bf998130d57e
Kaiya Xiong <xiongkaiya@gmail.com>
2019-02-01 13:57:21 +0800

    fix bug

--- startup.sh
+++ startup.sh
@@ -49,20 +49,23 @@ if [ -d "/var/lib/zerotier-one/moons.d" ] # check if the moons conf has generate
 then
         moon_id=$(cat /var/lib/zerotier-one/identity.public | cut -d ':' -f1)
         echo -e "Your ZeroTier moon id is \033[0;31m$moon_id\033[0m, you could orbit moon using \033[0;31m\"zerotier-cli orbit $moon_id $moon_id\"\033[0m"
-        /zerotier-one
+        #/zerotier-one
+	systemctl restart zerotier-one
 else
-        nohup /zerotier-one >/dev/null 2>&1 &
+	systemctl start zerotier-one
         # Waiting for identity generation...'
         while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
 	        sleep 1
         done
-        /zerotier-idtool initmoon /var/lib/zerotier-one/identity.public >>/var/lib/zerotier-one/moon.json
+        zerotier-idtool initmoon /var/lib/zerotier-one/identity.public >>/var/lib/zerotier-one/moon.json
         sed -i 's/"stableEndpoints": \[\]/"stableEndpoints": ['$stableEndpointsForSed']/g' /var/lib/zerotier-one/moon.json
-        /zerotier-idtool genmoon /var/lib/zerotier-one/moon.json > /dev/null
+        zerotier-idtool genmoon /var/lib/zerotier-one/moon.json > /dev/null
         mkdir /var/lib/zerotier-one/moons.d
         mv *.moon /var/lib/zerotier-one/moons.d/
-        pkill zerotier-one
+        #pkill zerotier-one
+	systemctl stop zerotier-one
         moon_id=$(cat /var/lib/zerotier-one/moon.json | grep \"id\" | cut -d '"' -f4)
         echo -e "Your ZeroTier moon id is \033[0;31m$moon_id\033[0m, you could orbit moon using \033[0;31m\"zerotier-cli orbit $moon_id $moon_id\"\033[0m"
-        exec /zerotier-one
+        #exec /zerotier-one
+	systemctl start zerotier-one
 fi
